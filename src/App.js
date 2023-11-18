7eeb3052d2500eaa6a9821d006540a76dd48a4af
Atahan Koc <koccata@gmail.com>
2023-11-18 18:32:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+   
+    state= {advice: ''}; // initial state 
+
+    componentDidMount(){
+     this.fetchAdvice(); // bileşen olusturuldugunda tavsiye cekme islemi gerceklesecek
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice') // make a request to the api
+        .then((response)=> {
+            const {advice} = response.data.slip; // api yanıtından tasviye metnini al
+            this.setState({advice}); // state'i güncelle
+        })
+        .catch((error)=>{
+             console.log(error); // hata varsa konsola yazdır
+        })
+     }
+   
+    render(){
+         const {advice} = this.state; // state'i destruct et
+
+        return(
+               <div className="app">
+                   <div className="card">
+                          <h1 className="heading">{advice}</h1>
+                          <button className="button" onClick={this.fetchAdvice}>
+                            <span>Give Me Advice!</span>
+                          </button>     
+               </div>
+                </div>
+            );
+    }
 }
 
 export default App;
